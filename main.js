6beb1b4a13e3639f58185191530a95a430f5403b
Amy-Rose <amyrlcollins@gmail.com>
2025-05-23 14:43:37 +0100

    added comments to explain code

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// || USER INPUT
-// alert("Hello world!");
-
-
 // || ROCK PAPER SCISSORS
 
 const start = confirm(`Do you want to play rock, paper, scissors? \n Best of three wins!`);
@@ -9,54 +5,61 @@ const start = confirm(`Do you want to play rock, paper, scissors? \n Best of thr
 console.log(start);
 
 if (start == true) {
-  let user;
+  let userChoice;
   let userScore = 0;
 
-  let computer;
+  let computerChoice;
   let computerScore = 0;
 
   let round = 1;
 
   const rps = {
-    choose: ["rock", "paper", "scissors"], //array to pick a choice from
+    choose: ["rock", "paper", "scissors"], //array for user to pick their play from: rock = 1, paper = 2, scissors = 3
 
-    // if user chooses this: computer chooses one of these[tie, user win, user loss]
-     rock: ["rock", "scissors", "paper"],
+    //user choice: [tie condition, win condition, lose condition]
+    rock: ["rock", "scissors", "paper"],
     paper: ["paper", "rock", "scissors"],
-     scissors: ["scissors", "paper", "rock"]
+    scissors: ["scissors", "paper", "rock"]
   }
 
-  while (userScore<3 && computerScore<3) {     
+  while (userScore<3 && computerScore<3) {   //while neither player has scored 3  
 
-    user = Number(prompt(`Round ${round}! \n Rock, paper or scissors? \n 1 = Rock \n 2 = Paper \n 3 = Scissors`));
+    userChoice = Number(prompt(`Round ${round}! 
+                              \n Rock, paper or scissors? 
+                              \n 1 = Rock \n 2 = Paper \n 3 = Scissors`));
 
-    while (!(Number.isInteger(user) && user>0 && user<=3)) { 
+    while (!(Number.isInteger(userChoice) && userChoice>0 && userChoice<=3)) { 
     //(!(conditions))
     //checking conditions that: user is an integer that's greater than 0 but less than 3.
     // any one of these conditions not met returns false, which the ! converts to true so the loop will run. 
     // if all conditions are met, returns true which the ! changes to false so the loop won't run/stops
 
-    user = Number(prompt(`You must enter a number between 1 and 3. \n Rock, paper or scissors? \n 1 = Rock \n 2 = Paper \n 3 = Scissors`));
+    userChoice = Number(prompt(`You must enter a number between 1 and 3. 
+                                \n Rock, paper or scissors? 
+                                \n 1 = Rock \n 2 = Paper \n 3 = Scissors`));
     } //end input validator while
 
-    user = rps.choose[user-1];
-    console.log(`User chooses ${user}.`);
+    userChoice = rps.choose[userChoice-1]; //matching user input to corresponding array value (choices 1-3, array entries 0-2)
+    console.log(`User chooses ${userChoice}.`);
 
-    computer = rps.choose[Math.floor(3 * Math.random())];
-    console.log(`Computer chooses ${computer}.`);
+    computerChoice = rps.choose[Math.floor(3 * Math.random())]; //generating a random number between 0 & 2 inclusive and using this to pick a play for the computer
+    console.log(`Computer chooses ${computerChoice}.`);
 
-    switch (computer) {
-      case rps[user][0]:
+    switch (computerChoice) { //if computerChoice
+      // = userChoice
+      case rps[userChoice][0]: //rps[userChoice] selects key in rps that points to appropriate tie/win/lose conditions for this round
         alert(`It's a tie! \n You - Computer \n ${userScore} - ${computerScore}`)
       break; 
 
-      case rps[user][1]:
+      // = win condition for user
+      case rps[userChoice][1]:
         userScore++;
         alert(`You win this round! \n You - Computer \n ${userScore} - ${computerScore}`)      
       
       break;
 
-      case rps[user][2]:
+      // = lose condition for user
+      case rps[userChoice][2]:
         computerScore++;
         alert(`Computer wins this round! \n You - Computer \n ${userScore} - ${computerScore}`)
         
@@ -65,11 +68,11 @@ if (start == true) {
 
       default:
         console.log(`Something has gone wrong :(`)
-    } //end switch
+    } //end switch (choice comparisons)
 
     round++;
 
-  } //end scores <3 while
+  } //end while (scores < 3)
 
   if (userScore>computerScore) {
     alert(`Congratulations! You beat the computer. Our AI overlords will remember this.`)
@@ -79,7 +82,7 @@ if (start == true) {
   }
 
 
-}//end if user wants to play game
+}//end if (user wants to play game)
 
 else {
   alert(`Maybe some other time!`)
